76b7cd79c29169ee0a4db1ee5d1aacfbd2e46020
Zaw Than <zawlurnthan@gmail.com>
2023-07-25 23:03:01 -0400

    edit Admin look and feel

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -9,7 +10,7 @@ from .models import Choice, Question
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 #     context = {'latest_question_list': latest_question_list }
-#     return render(request, 'polls/index.html', context)
+#     return render(request, 'polls/base_site.html', context)
 #
 #
 # def detail(request, question_id):
@@ -28,8 +29,10 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        # return the last five published question
-        return Question.objects.order_by('-pub_date')[:5]
+        # return the last five published questions (not including those set to be published in the future).
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -37,6 +40,13 @@ class DetailView(generic.DetailView):
     # context variable as question is provided to the template automatically by Django Generic view class.
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        :return:
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
